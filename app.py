6f2bec41c938b188a1a1f98d1fa09a45f15fbdac
joescope <joescope@terpmail.umd.edu>
2023-03-28 12:57:26 -0400

    Added form fields to favorite-course.html and added root to app.py. Also created contact.html.

--- app.py
+++ app.py
@@ -12,15 +12,27 @@ def hello_world():  # put application's code here
 def hello():  # put application's code here
     return render_template('hello.html')
 
+
 @app.route('/about')
 def about():  # put application's code here
     return render_template('about.html')
 
-@app.route('/favorite-course')
-def favoritecourse():  # put application's code here
-    print('Course Subject: ' + request.args.get('course_subject'))
-    print('Course Number: ' + request.args.get('course_number'))
+
+@app.route('/favorite-course', methods=['GET', 'POST'])
+def favoritecourse():
+    if request.method == 'POST':
+        print('Course Subject: ' + request.form.get('course_subject'))
+        print('Course Number: ' + request.form.get('course_number'))
+
     return render_template('favorite-course.html')
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
